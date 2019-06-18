c692364011599c7879901e7dd8678751ac8b058e
Pavel ishenin <isheninp@gmail.com>
2019-06-18 20:19:34 +0300

    1806

--- app/controllers/spree/admin/coupons_import_controller.rb
+++ app/controllers/spree/admin/coupons_import_controller.rb
@@ -10,9 +10,13 @@ module Spree
       def create
         filename=params[:coupons_import][:file].path  
         keys = File.readlines(filename)
-        product = Spree::Product.find params[:coupons_import][:product]
-        variants = Spree::Variant.where('product_id=?', product.id)
-        end_z = Spree::SalesSchedule.where(variant_id: variants).order(:end_time).max[:end_time] rescue product.discontinue_on 
+        products = []
+        params[:coupons_import][:product].split(',').each do |product_id|
+          products << Spree::Product.find(product_id)
+        end
+        variants = Spree::Variant.where(product_id: products.pluck(:id))
+        end_promo = Spree::SalesSchedule.where(variant_id: variants).order(:end_time).max[:end_time] rescue products.map{|p| p.discontinue_on}.max
+        start_promo = products.map{ |p| p.available_on }.min
         percent = params[:coupons_import][:percent].to_i
         @results = []
         keys.each do |c|
@@ -23,8 +27,8 @@ module Spree
               description: params[:coupons_import][:promo_desc],
               match_policy: 'all',
               usage_limit: '1',
-              starts_at: product.available_on,
-              expires_at: end_z,
+              starts_at: start_promo,
+              expires_at: end_promo,
               code: code
             })
             promotion.promotion_actions << Spree::Promotion::Actions::CreateAdjustment.create({
@@ -35,7 +39,7 @@ module Spree
             })
             promotion_rule.promotion = promotion
             promotion_rule.save  
-            promotion_rule.products << product
+            promotion_rule.products << products
             @results << {code: code, status: true}
           else
             @results << {code: code, status: false}
